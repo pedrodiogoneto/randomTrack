cc840013e93361ce99a03bfc655fb6c7146c5b4c
pedrodiogoneto <pedritoneto@gmail.com>
2021-01-10 22:30:56 +0100

    fix: Fix homepage header

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -30,7 +30,7 @@ const Homepage = () => {
 
     return (
         <HomepageWrapper>
-            <HeaderBrand>SHOWME</HeaderBrand>
+            <HeaderBrand>SHOWMESOMETHING</HeaderBrand>
             {track && !error && <MusicInfo track={track}/>}
             <ButtonWrapper track={track?.trackId}>
                 {!loading && <Button onClick={requestMusic}>SHOW ME SOMETHING NEW</Button>}
